643a6a283a9b7add8d86e81b4693fed0f60898d8
Ilyas Boudhaine <ilyasboudhaine1@gmail.com>
2024-01-06 17:56:08 +0100

    field view

--- Controllers/fieldController.php
+++ Controllers/fieldController.php
@@ -11,8 +11,29 @@ class FieldController {
 
     public function displayFieldsByFarmID($farmID) {
         $fieldDAO = $this->daoFactory->createFieldDAO();
+        $cropDAO = $this->daoFactory->createCropDAO();
 
         $fields = $fieldDAO->getFieldsByFarmID($farmID);
+
+        $fieldsCropsMap = [];
+
+        foreach ($fields as $field) {
+            $fieldID = $field->getFieldID();
+            $cropsMap = $fieldDAO->getFieldCrops($fieldID);
+
+            $enhancedCropsMap = [];
+            foreach ($cropsMap as $cropID => $area) {
+                $cropInfo = $cropDAO->getCropByID($cropID);
+                if ($cropInfo) {
+                    $enhancedCropsMap[$cropID] = [
+                        'cropType' => $cropInfo->getCropType(),
+                        'area' => $area,
+                    ];
+                }
+            }
+
+            $fieldsCropsMap[$fieldID] = $enhancedCropsMap;
+        }
 
         include(__DIR__ . '/../Views/fields.php');
     }
