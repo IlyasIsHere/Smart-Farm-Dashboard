490138f599307cdda8e9dd4355173ee8fa03e18e
Ilyas Boudhaine <ilyasboudhaine1@gmail.com>
2024-01-06 02:04:29 +0100

    added additional farmDAO methods

--- DAOs/Implementations/farmDAOImpl.php
+++ DAOs/Implementations/farmDAOImpl.php
@@ -39,7 +39,74 @@ class FarmDAO implements FarmDAOInterface {
         }
     }
 
-    // TO DO: Additional methods
+    // Delete a farm by ID
+    public function deleteFarm($farmID) {
+        $stmt = $this->db->prepare("DELETE FROM Farm WHERE farmID = ?");
+        $stmt->bind_param("i", $farmID);
+
+        if ($stmt->execute()) {
+            // Successfully deleted
+            return true;
+        } else {
+            // TO DO: Handle error
+            return false;
+        }
+    }
+
+    // Update farm name by ID
+    public function updateFarmName($farmID, $name) {
+        $stmt = $this->db->prepare("UPDATE Farm SET name = ? WHERE farmID = ?");
+        $stmt->bind_param("si", $name, $farmID);
+
+        if ($stmt->execute()) {
+            // Successfully updated
+            return true;
+        } else {
+            // TO DO: Handle error
+            return false;
+        }
+    }
+
+    // Update farm localisation by ID
+    public function updateFarmLocalisation($farmID, $latitude, $longitude) {
+        $stmt = $this->db->prepare("UPDATE Farm SET latitude = ?, longitude = ? WHERE farmID = ?");
+        $stmt->bind_param("ddi", $latitude, $longitude, $farmID);
+
+        if ($stmt->execute()) {
+            // Successfully updated
+            return true;
+        } else {
+            // TO DO: Handle error
+            return false;
+        }
+    }
+
+    // Update farm owner by ID
+    public function updateFarmOwner($farmID, $ownerName) {
+        $stmt = $this->db->prepare("UPDATE Farm SET ownerName = ? WHERE farmID = ?");
+        $stmt->bind_param("si", $ownerName, $farmID);
+
+        if ($stmt->execute()) {
+            // Successfully updated
+            return true;
+        } else {
+            // TO DO: Handle error
+            return false;
+        }
+    }
+
+    // Get all farms
+    public function getAllFarms() {
+        $result = $this->db->query("SELECT * FROM Farm");
+
+        $farms = array();
+
+        while ($row = $result->fetch_assoc()) {
+            $farms[] = new Farm($row['farmID'], $row['name'], $row['ownerName'], $row['email'], $row['phoneNum'], $row['latitude'], $row['longitude']);
+        }
+
+        return $farms;
+    }
 }
 
 ?>
--- DAOs/Interfaces/farmDAO.php
+++ DAOs/Interfaces/farmDAO.php
@@ -7,7 +7,20 @@ interface FarmDAOInterface {
     // Get farm by ID
     public function getFarmByID($farmID);
 
-    // TO DO: Additional methods
+    // Delete a farm by ID
+    public function deleteFarm($farmID);
+
+    // Update farm name by ID
+    public function updateFarmName($farmID, $name);
+
+    // Update farm localisation by ID
+    public function updateFarmLocalisation($farmID, $latitude, $longitude);
+
+    // Update farm owner by ID
+    public function updateFarmOwner($farmID, $ownerName);
+
+    // Get all farms
+    public function getAllFarms();
 }
 
 ?>
